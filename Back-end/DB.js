480d302f769a89f52b7891b0e0902d09bb5ab02d
Shalini Jangid <shalinijangid111@gmail.com>
2025-06-19 16:59:21 +0530

    test

--- Back-end/DB.js
+++ Back-end/DB.js
@@ -4,8 +4,7 @@ const asyncHandler = require('express-async-handler');
 const connectDB = asyncHandler(async () => {
   const conn = await mongoose.connect(process.env.MONGO_URL, {
     
-  });
-
+});
   console.log(`MongoDB Connected`);
 });
 
